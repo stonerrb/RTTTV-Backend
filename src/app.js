92024ffadf6bc5d87b18feb98cf896d2300489ae
Rushabh Babre <babrerushabh1@gmail.com>
2024-01-05 18:06:24 +0530

    demo

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+require('dotenv').config()
+
+const User = require('./models/user');
+
 const express = require('express');
 const cors = require('cors');
 
@@ -6,6 +10,15 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+
+app.get('/get-users', (req, res) => {
+    try{
+        
+    }
+    catch(e){
+
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
