9879c9f5dad0c18822cd8f4384c0544999f46886
Rushabh Babre <babrerushabh1@gmail.com>
2024-01-06 23:43:01 +0530

    fix3

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const authroute = require('./routes/auth.js')
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
