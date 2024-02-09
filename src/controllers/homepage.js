dc7c9cb38e399740a811009c1422163af05040af
Rushabh Babre <babrerushabh1@gmail.com>
2024-02-09 14:07:56 +0530

    Added Link

--- src/controllers/homepage.js
+++ src/controllers/homepage.js
@@ -4,14 +4,19 @@ const homepage = async (req, res) => {};
 
 const getMovie = async (req, res) => {
   const _id = req.params.id;
-  
+  const link = "https://rttv-movies.s3.amazonaws.com/" + _id + ".mp4";
   try {
     const movie = await Movie.findById(_id);
     if (!movie) {
       return res.status(404).send("Send Valid Id");
     }
 
-    res.status(200).send(movie);
+    const movieFill = {
+      ...movie.toObject(),
+      link: link
+    };   
+     
+    res.status(200).send(movieFill);
   } catch (e) {
     res.status(500).send(e);
   }
